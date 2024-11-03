ca6dcf269041cbfb780c265fbff43ee64ae0ca1c
Jeremiah Williams <williamsjeremiahkenneth@gmail.com>
2024-11-03 16:45:43 -0500

    This commit is to fix the function name per Shawn's comment

--- src/components/MissionStatement.tsx
+++ src/components/MissionStatement.tsx
@@ -1,4 +1,4 @@
-export default function Example() {
+export default function MissionStatement() {
     return (
       <div className="bg-neutral-50 px-6 py-24 sm:py-32 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
