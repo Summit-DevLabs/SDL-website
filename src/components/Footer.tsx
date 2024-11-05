a7d1859394b3bf7633b2b6a23ffc1a1cb26bcb9e
Shawn Wilson <shawnwilsontrpt@gmail.com>
2024-11-05 18:07:10 -0500

    footer name change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -110,7 +110,7 @@ export function Footer() {
             <Logo className="h-8" fillOnHover />
           </Link>
           <p className="text-sm text-neutral-700">
-            © Studio Agency Inc. {new Date().getFullYear()}
+            © Summit DevLabs {new Date().getFullYear()}
           </p>
         </div>
       </FadeIn>
