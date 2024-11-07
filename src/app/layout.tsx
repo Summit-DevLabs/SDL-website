456fd85a410d315ed89f1a4c2dbc96c92b788a68
Jeremiah Williams <williamsjeremiahkenneth@gmail.com>
2024-11-07 13:09:18 -0500

    Added metadata to correct area on layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { type Metadata } from 'next'
-import Head from 'next/head'
 import { RootLayout } from '@/components/RootLayout'
 import '@/styles/tailwind.css'
 
@@ -8,16 +7,20 @@ export const metadata: Metadata = {
     template: 'Summit DevLabs - %s',
     default: 'Summit DevLabs - Home',
   },
+  openGraph: {
+    images: [
+      {
+        url: '/images/assets/SDL_Logo_float.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
-      <Head>
-        <meta property="og:image" content="@images/assets/SDL_Logo_float.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-      </Head>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
