92ec7a0e62ea6061c7ddfb0d98e1d453f3c3f645
Jeremiah Williams <williamsjeremiahkenneth@gmail.com>
2024-11-07 11:55:37 -0500

    Added correct favicon and updated text link preview picture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import { type Metadata } from 'next'
-
+import Head from 'next/head'
 import { RootLayout } from '@/components/RootLayout'
-
+import logo from '@/images/assets/SDL_Logo_float.png'
 import '@/styles/tailwind.css'
+import { Logo } from '@/components/Logo'
 
 export const metadata: Metadata = {
   title: {
@@ -14,6 +15,11 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
+      <Head>
+        <meta property="og:image" content="@images/assets/SDL_Logo_float.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
