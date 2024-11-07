bc90270eeef84796cd3cc91c2d27c74fff3bb3b6
Jeremiah Williams <williamsjeremiahkenneth@gmail.com>
2024-11-07 13:26:58 -0500

    Trying putting meta in a new head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,24 @@
 import { type Metadata } from 'next'
 import { RootLayout } from '@/components/RootLayout'
 import '@/styles/tailwind.css'
+import logo from '@/images/assets/SDL_Logo_float.png'
 
 export const metadata: Metadata = {
   title: {
     template: 'Summit DevLabs - %s',
     default: 'Summit DevLabs - Home',
   },
-  openGraph: {
-    images: [
-      {
-        url: '/images/assets/SDL_Logo_float.png',
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
+      <head>
+        <meta property="og:title" content="Summit DevLabs - Home" />
+        <meta property="og:description" content="Welcome to Summit DevLabs" />
+        <meta property="og:image" content={logo.src} />
+        <meta property="og:url" content="https://summitdevlabs.com" />
+      </head>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
